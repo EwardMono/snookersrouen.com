1b025ba3caa06934f04982a19af27cf9c618bf0b
EwardMono <147155594+EwardMono@users.noreply.github.com>
2023-10-07 16:29:41 +0100

    Add files via upload

---
+++ snooker.js
@@ -0,0 +1,7 @@
+let overTwentyOne = document.getElementById('twenty-older')
+let underTwentyOne = document.getElementById('twenty-under')
+
+
+underTwentyOne.onclick = () => alert('Sorry your not old enough to access this site')
+
+overTwentyOne.onclick = () => alert('Welcome Snookers Rouem')
